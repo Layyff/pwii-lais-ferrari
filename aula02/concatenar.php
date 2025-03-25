f06f1277ea02bb6358a5b71f9646f84c84b35c66
Laís Ferrari <ferrarilais6@gmail.com>
2025-03-25 16:31:35 -0300

    Cria um arquivo de concatenação

---
+++ aula02/concatenar.php
@@ -0,0 +1,14 @@
+<?php
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    EcHo "Hello World!<br>";
+
+    $nome = "Laís";
+    $sobrenome = "Ferrari";
+    $numero = 8;
+
+    echo $nome . " lindo" . "<br>";
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p> " . $nome . 
+    " " . $sobrenome . "</p>";
+?>
